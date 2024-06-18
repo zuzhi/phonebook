0c057972f934a7a560b2aede451bd6dbdf24614c
zuzhi <zuzhi.hu@gmail.com>
2024-06-18 13:37:15 +0800

    axios, useEffect

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,27 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 import Filter from './components/Filter'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 
+import axios from 'axios'
+
 function App() {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ filtered, setFiltered ] = useState(persons)
   const [ q, setQ ] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+        setFiltered(response.data)
+      })
+  }, [])
 
   const handleSubmit = (event) => {
     event.preventDefault()
@@ -27,6 +34,7 @@ function App() {
     const nameExists = persons.filter(person => person.name === newName).length > 0
     if (nameExists) {
       alert(`${newName} is already added to phonebook`)
+      return
     }
 
     const newPersons = persons.concat(person)
