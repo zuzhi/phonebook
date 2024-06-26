47b589c9dd31fe74f1c545f1dd486ac0317aab14
zuzhi <zuzhi.hu@gmail.com>
2024-06-26 10:29:29 +0800

    command-line database

---
+++ backend/mongo.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url =
+  `mongodb+srv://fullstack:${password}@cluster0.j14gmlk.mongodb.net/phonebook?retryWrites=true&w=majority&appName=Cluster0`
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (name && number) {
+  const person = new Person({ name: name, number: number })
+  person.save().then(() => {
+    console.log(`Added ${name} number ${number} to phone book`)
+    mongoose.connection.close()
+  })
+} else {
+  Person.find({}).then(result => {
+    console.log("phonebook:")
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+  })
+}
