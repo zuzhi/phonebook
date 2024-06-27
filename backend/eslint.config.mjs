6935f45828a19c5a6c6bab3ad436d1ffb68ffbc9
zuzhi <zuzhi.hu@gmail.com>
2024-06-27 12:01:18 +0800

    fix eslint errors

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,51 +1,54 @@
 import globals from 'globals';
-import pluginJs from "@eslint/js";
 import stylisticJs from '@stylistic/eslint-plugin-js'
+import pluginJs from "@eslint/js";
 
 export default [
-    {
-        ignores: ['dist/']
+  pluginJs.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: {
+        ...globals.node,
+      },
+      ecmaVersion: "latest",
     },
-    {
-        files: ["**/*.js"],
-        languageOptions: {
-            sourceType: "commonjs",
-            globals: globals.node
-        },
-        plugins: {
-            '@stylistic/js': stylisticJs
-        },
-        rules: {
-            '@stylistic/js/indent': [
-                'error',
-                2
-            ],
-            '@stylistic/js/linebreak-style': [
-                'error',
-                'unix'
-            ],
-            '@stylistic/js/quotes': [
-                'error',
-                'single'
-            ],
-            '@stylistic/js/semi': [
-                'error',
-                'never'
-            ],
-            'eqeqeq': 'error',
-            'no-trailing-spaces': 'error',
-            'object-curly-spacing': [
-                'error',
-                'always'
-            ],
-            'arrow-spacing': [
-                'error', {
-                    'before': true,
-                    'after': true
-                }
-            ],
-            'no-console': 0
+    plugins: {
+      '@stylistic/js': stylisticJs,
+    },
+    rules: {
+      '@stylistic/js/indent': [
+        'error',
+        2
+      ],
+      '@stylistic/js/linebreak-style': [
+        'error',
+        'unix'
+      ],
+      '@stylistic/js/quotes': [
+        'error',
+        'single'
+      ],
+      '@stylistic/js/semi': [
+        'error',
+        'never'
+      ],
+      'eqeqeq': 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': [
+        'error',
+        'always'
+      ],
+      'arrow-spacing': [
+        'error', {
+          'before': true,
+          'after': true
         }
+      ],
+      'no-console': 'off',
     },
-    pluginJs.configs.recommended,
+  },
+  {
+    ignores: ['dist/**', "build/**"]
+  },
 ];
